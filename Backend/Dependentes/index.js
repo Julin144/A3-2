ea5619cda6c396d8c3060d3e4001ad3675fd30a9
juliallopes12 <julialllopes1233@gmail.com>
2022-10-23 14:23:41 -0300

    Subir Barramento de Eventos

--- Backend/Dependentes/index.js
+++ Backend/Dependentes/index.js
@@ -1,23 +1,40 @@
 const express = require('express');
 const app = express();
 const bodyParse = require('body-parser');
+const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
 
-const Dependentes = {};
-let contador = 0;
+const dependentesPorPacieneteId = {};
 
 app.use(bodyParse.json());
 
-app.get('/dependente', (req, res) => {
-    res.send(Dependentes)
+app.get('paciente/id:/dependente', (req, res) => {
+    res.send(dependentesPorPacieneteId[req.params.id] || [])
 });
 
-app.put('/dependente', (req, res) => {
-    contador++;
-    const { CPFUsuario, nome, sobrenome, CPFPaciente, numeroCarteirinha } = req.body;
-    Dependentes[contador] = {
-        contador, CPFUsuario, nome, sobrenome, CPFPaciente, numeroCarteirinha
-    }
-    res.status(201).send(Dependentes[contador]);
+app.put('paciente/id:/dependente', async (req, res) => {
+    const idDependentes = uuidv4();
+    const { nome, sobrenome, CPFPaciente, numeroCarteirinha } = req.body;
+
+    dependentesPorPacieneteId =
+        dependentesPorPacieneteId[req.params.id] || [];
+    dependentesPorPacieneteId.push({ id: idDependentes, nome, sobrenome, CPFPaciente, numeroCarteirinha });
+    dependentesPorPacieneteId[req.params.id] =
+        dependentesPorPacieneteId;
+
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "ObservacaoCriada",
+        dados: {
+            id: idDependentes,  nome, sobrenome, CPFPaciente, numeroCarteirinha, pacienteId: req.params.id
+        }
+    })
+
+    res.status(201).send(dependentesPorPacieneteId);
+});
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok" });
 });
 
 app.listen(5000, () => {
